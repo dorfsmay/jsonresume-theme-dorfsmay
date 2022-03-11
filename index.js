00a8a077d413b054ad8de0252647c9bef13a7c7c
Yves Dorfsman <yves@zioup.com>
2022-03-11 15:49:35 -0700

    backup

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-//const Handlebars = require("handlebars");
+const fs = require("fs");
+const path = require("path");
+const Handlebars = require("handlebars");
 
 function render(_resume) {
-  return "<p>Hello !!</p>";
+  const resumeTemplate = fs.readFileSync(
+    path.join(__dirname, "resume.hbs"),
+    "utf-8"
+  );
+  return Handlebars.compile(resumeTemplate)({});
 }
 
 module.exports = {
