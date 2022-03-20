187b31ca0b19e2c8de2eb0dda62adfef975ba586
Yves Dorfsman <yves@zioup.com>
2022-03-20 09:25:15 -0600

    backup

---
+++ README.md
@@ -0,0 +1,4 @@
+
+* `nvm use node`
+* `npm install -g resume-cli`
+* `clear ; resume serve --resume ./yourResume.json --theme jsonresume-theme-dorfsmay`
--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const fs = require("fs");
 const path = require("path");
-const Handlebars = require("handlebars");
+const sass = require("sass");
+const handlebars = require("handlebars");
 
-function render(_resume) {
-  const resumeTemplate = fs.readFileSync(
-    path.join(__dirname, "resume.hbs"),
-    "utf-8"
-  );
-  return Handlebars.compile(resumeTemplate)({});
+function render(resume) {
+  const PARTIALS_DIR = path.join(__dirname, "partials");
+  fs.readdirSync(PARTIALS_DIR).map((fn) => {
+    const matches = /^([^.]+).hbs$/.exec(fn);
+    if (matches) {
+      const name = matches[1];
+      const content = fs.readFileSync(path.join(PARTIALS_DIR, fn), "utf8");
+      handlebars.registerPartial(name, content);
+    }
+  });
+
+  const template = fs.readFileSync(path.join(__dirname, "resume.hbs"), "utf-8");
+  const css = sass.compile(path.join(__dirname, "style.scss")).css;
+  console.log(css);
+  return handlebars.compile(template)({ resume: resume, css: css });
 }
 
 module.exports = {
